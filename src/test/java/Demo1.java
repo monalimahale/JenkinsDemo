5008a0449123b8ca1b8cdaa7e77dd846e7558bbf
Monali Mahale <monalimahale11@gmail.com>
2019-09-14 17:50:15 +0530

    first commit

---
+++ src/test/java/Demo1.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+        @Test
+
+        public void testgooglesearch(){
+
+        WebDriver driver = new ChromeDriver();
+
+//it will open the goggle page
+
+        driver.get("http://google.in");
+
+//we expect the title “Google “ should be present
+
+        String Expectedtitle = "Google";
+
+//it will fetch the actual title
+
+        String Actualtitle = driver.getTitle();
+
+        System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+
+//it will compare actual title and expected title
+
+        Assert.assertEquals(Actualtitle, Expectedtitle);
+
+//print out the result
+        System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+    }
+
+}
+
